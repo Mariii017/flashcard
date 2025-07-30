aae3585d2e7b4a8821c8cfc8ca3169d48b4da5e8
Mariii017 <mariana.rocombach@escola.pr.gov.br>
2025-07-30 12:52:36 +0000

    criamos as  perguntas no javascript

---
+++ perguntas.js
@@ -0,0 +1,12 @@
+criacartao(
+"inteligencia artificial",
+"a inteligencia artificial ira substituir todos os empregos humanos?",
+"alguns especialistas acreditam que, em vez de eliminar empregos, a ia criara novas oportunidades exigindo que os profissionais se adaptem e adquiram novas habilidades"
+)
+criacartao(
+    "inteligencia artificial",
+    "estamos dependentes demais da tecnologia?",
+    "pense no seu dia a dia, quantas vezes voce verifica o celular ou usa aplicativos para tarefas simples. A nomofobia,omedo de ficar sem o celular esta se tornando um problema real "
+)
+
+
