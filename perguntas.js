57cfd9429abed66c386947885847f0ab9118e2e9
Mariii017 <mariana.rocombach@escola.pr.gov.br>
2025-08-13 12:52:26 +0000

    bbb

--- perguntas.js
+++ perguntas.js
@@ -8,5 +8,29 @@ criacartao(
     "estamos dependentes demais da tecnologia?",
     "pense no seu dia a dia, quantas vezes voce verifica o celular ou usa aplicativos para tarefas simples. A nomofobia,omedo de ficar sem o celular esta se tornando um problema real "
 )
+criacartao(
+    "inteligencia artificial",
+    "como a inteligencia artificial aprende",
+    "através da exposição a grandes quantidades de dados e utilizando algoritmos para identificar padrões e relações"
+)
+criacartao(
+    "curiosidade do cachorro"
+    "qual tempo de vida de um cachorro",
+    "através da exposição a grandes quantidades de dados e utilizando algoritmos para identificar padrões e relações"
+)
+criacartao(
+    "curiosidade humana"
+   "quanto tempo podemos ficar sem tomar agua",
+   "cerca de 2 a 4 dias, embora o tempo exato varie dependendo de fatores como temperatura, atividade física e saúde individual"
+)
+criacartao(
+    "ia para estudo"
+"qual a melhormaneira de usar ia"
+"os sistemas de IA se tornam cada vez mais competentes para realizar tarefas específicas, como reconhecer imagens, traduzir idiomas e muito mais", 
 
-
+)
+criacartao (
+    "voltar ao passado seria bom"
+    "sem ia no mundo",
+    "A organização alertou que quase metade dos empregos em todo o mundo poderiam ser afetados. Enquanto a IA transforma as economias, criando uma variedade de oportunidades, também traz riscos de aprofundar as desigualdades. "
+)
